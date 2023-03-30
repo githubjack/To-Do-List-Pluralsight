cc3fbe32cc43729fe2fc454579664d8f5c74ac29
githubjack <jack.ui.maynard@gmail.com>
2023-03-30 14:40:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# To-Do-List-Pluralsight
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-3dcstb)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>To Do List App</h1>
+    </div>
+  );
+}
